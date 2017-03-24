8e05971e707e252c7ca160058bf8eab1459848e9
Alessandro <gort818@gmail.com>
2017-03-24 06:51:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon(QLatin1String("/home/alessandro/netflix/data/netflix.svg")));
+    app.setWindowIcon(QIcon(QLatin1String("data/netflix.svg")));
     MainWindow w;
    // QWebEngineView view;
     //view.setUrl(QUrl(QStringLiteral("http://netflix.com")));
