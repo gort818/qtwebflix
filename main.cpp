310b3c8f9abddb7f5c6f203a1d9a7f9658c5f75a
Alessandro <gort818@gmail.com>
2017-10-27 20:08:35 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon(QLatin1String("data/netflix.svg")));
+    app.setWindowIcon(QIcon(QLatin1String("netflix")));
     MainWindow w;
    
     w.show();
