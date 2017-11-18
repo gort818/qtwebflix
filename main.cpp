3618d553c1bb645e507fcb9fc5fbe833ba3b9789
Alessandro <gort818@gmail.com>
2017-11-17 17:11:47 -0800

    remove call to window maximize
    Window now saves state on exit

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(QLatin1String("netflix")));
     MainWindow w;
-   
-    w.setWindowState(Qt::WindowMaximized);
     w.show();
     return app.exec();
 }
