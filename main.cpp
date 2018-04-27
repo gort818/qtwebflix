c46f53703ff9c9e9badf890ef43021229ee89dad
Alessandro <gort818@gmail.com>
2018-04-26 18:09:12 -0700

    Remove command line help option
    Remove the recently added command line help option as it breaks qtwebengine command line options.

--- main.cpp
+++ main.cpp
@@ -10,15 +10,6 @@
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
 
-  QCommandLineParser parser;
-  parser.setApplicationDescription(
-      "\nQtwebFlix Help\n\n If you are having trouble accessing DRM content "
-      "please add this commandline option:\n"
-      "--register-pepper-plugins=\"/usr/lib/qt/plugins/ppapi/"
-      "libwidevinecdmadapter.so; application/x-ppapi-widevine-cdm\"");
-  parser.addHelpOption();
-
-  parser.process(QApplication::arguments());
 
   MainWindow w;
 
